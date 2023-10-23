c795d3bdc0afa0f8d644681989d04b82861fb535
christianjramirez <104547521+christianjramirez@users.noreply.github.com>
2023-10-22 22:54:42 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pyqrcode
+import tkinter
+from tkinter import ttk
 import sys
+
+
 ssid = input("Please enter network id: ")
 security = 'WPA'
 password = input("Please Enter password: ")
 qr = pyqrcode.create(f'WIFI:S:{ssid};T:{security};P:{password};;')
-print(qr.terminal(quiet_zone=1))
+
+code_xbm = qr.xbm(scale=5)
+##Creates window
+top = tkinter.Tk()
+top.title('qr_code_gen')
+
+##Creates bitmap of qr code
+code_bmp = tkinter.BitmapImage(data=code_xbm)
+code_bmp.config(background="white")
+
+label = tkinter.Label(image=code_bmp)
+label.pack()
+top.mainloop()
